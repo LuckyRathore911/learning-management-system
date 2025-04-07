8a15f490a6ec828a8c1cec4866b7ce17d5d9ec97
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2025-04-07 22:24:56 +0530

    created home page

--- client/src/pages/learner/Home.jsx
+++ client/src/pages/learner/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Introduction from "../../components/learner/Introduction";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col items-center space-y-7 text-center">
+      <Introduction />
+    </div>
+  );
 };
 
 export default Home;
