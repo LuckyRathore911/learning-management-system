6dd17c5bbc2dde1ce76f73de4fd010ac364a284d
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2025-04-08 22:50:26 +0530

    added courses section

--- client/src/components/learner/CoursesSection.jsx
+++ client/src/components/learner/CoursesSection.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+
+import { AppContext } from "../../context/AppContext";
+import CourseCard from "./CourseCard";
 
 const CoursesSection = () => {
-  return (
-    <div>
-      CoursesSection
-    </div>
-  )
-}
+  const { allCourses } = useContext(AppContext);
 
-export default CoursesSection
+  return (
+    <div className="py-16 md:px-40 px-8">
+      <h2 className="text-3xl font-medium text-gray-800">
+        Learn from the best
+      </h2>
+      <p className="text-sm md:text-base text-base-500 mt-3">
+        Discover courses across various categories.
+      </p>
+      <div className="grid [grid-template-columns:repeat(auto-fit,minmax(200px,1fr))] px-4 md:px-0 md:my-16 my-10 gap-4">
+        {allCourses.slice(0, 4).map((course, index) => (
+          <CourseCard key={index} course={course} />
+        ))}
+      </div>
+
+      <Link
+        to={"/courses"}
+        onClick={() => scrollTo(0, 0)}
+        className="text-gray-500 border border-gray-500/30 px-10 py-3 rounded"
+      >
+        Show all courses
+      </Link>
+    </div>
+  );
+};
+
+export default CoursesSection;
--- client/src/pages/learner/Home.jsx
+++ client/src/pages/learner/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Introduction from "../../components/learner/Introduction";
+import CoursesSection from "../../components/learner/CoursesSection";
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center space-y-7 text-center">
       <Introduction />
+      <CoursesSection />
     </div>
   );
 };
