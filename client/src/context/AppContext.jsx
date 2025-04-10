946736737b2f16f34c971a36af6bdee8ee65deb0
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2025-04-11 00:04:36 +0530

    added left portion of course-details section

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from "humanize-duration";
 
 import { dummyCourses } from "../assets/assets";
 
@@ -27,6 +28,37 @@ export const AppContextProvider = props => {
     return totalRating / course.courseRatings.length;
   };
 
+  //calculate course chapter time
+  const calculateChapterTime = chapter => {
+    let time = 0;
+    chapter.chapterContent.map(lecture => {
+      time += lecture.lectureDuration;
+    });
+    return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] });
+  };
+
+  //calculate course duration
+  const calculateCourseDuration = course => {
+    let time = 0;
+    course.courseContent.map(chapter => {
+      chapter.chapterContent.map(lecture => {
+        time += lecture.lectureDuration;
+      });
+    });
+    return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] });
+  };
+
+  //calculate number of lectures in a course
+  const calculateNumberOfLectures = course => {
+    let totalLectures = 0;
+    course.courseContent.forEach(chapter => {
+      if (Array.isArray(chapter.chapterContent)) {
+        totalLectures += chapter.chapterContent.length;
+      }
+    });
+    return totalLectures;
+  };
+
   useEffect(() => {
     fetchAllCourses();
   }, []);
@@ -35,9 +67,12 @@ export const AppContextProvider = props => {
     currency,
     allCourses,
     navigate,
-    calculateRating,
     isAdmin,
     setIsAdmin,
+    calculateRating,
+    calculateChapterTime,
+    calculateCourseDuration,
+    calculateNumberOfLectures,
   };
 
   return (
