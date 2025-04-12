66d68e104a061767479e19835330602cbe9958bf
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2025-04-12 18:48:57 +0530

    added admin sidebar and footer

--- client/src/pages/admin/Admin.jsx
+++ client/src/pages/admin/Admin.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 import Navbar from "../../components/admin/Navbar";
+import SideBar from "../../components/admin/SideBar";
+import Footer from "../../components/admin/Footer";
 
 const Admin = () => {
   return (
     <div className="text-default min-h-screen bg-white">
       <Navbar />
-      <div>{<Outlet />}</div>
+
+      <div className="flex">
+        <SideBar />
+        <div className="flex-1"> {<Outlet />}</div>
+      </div>
+
+      <Footer />
     </div>
   );
 };
