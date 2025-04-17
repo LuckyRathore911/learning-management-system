5c529ddb451c6356a9cf6fece10ee1bf853ea224
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2025-04-17 21:17:57 +0530

    added stripe webhook management

--- server/main.js
+++ server/main.js
@@ -5,7 +5,7 @@ import { clerkMiddleware } from "@clerk/express";
 
 import connectDB from "./configs/mongodb.js"; //file extension is mandatory
 import connectCloudinary from "./configs/cloudinary.js";
-import { clerkWebhooks } from "./controllers/webhooks.js";
+import { clerkWebhooks, stripeWebhooks } from "./controllers/webhooks.js";
 import adminRouter from "./routes/adminRoutes.js";
 import courseRouter from "./routes/courseRoutes.js";
 import userRouter from "./routes/userRoutes.js";
@@ -29,6 +29,7 @@ app.post("/clerk", express.json(), clerkWebhooks);
 app.use("/api/admin", express.json(), adminRouter);
 app.use("/api/course", express.json(), courseRouter);
 app.use("/api/user", express.json(), userRouter);
+app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
 //port
 const PORT = process.env.PORT || 5000;
