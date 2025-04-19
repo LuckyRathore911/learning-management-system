646213b2ad94b842225647b747c9fce82af943d8
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2025-04-19 17:55:24 +0530

    removed dummy assets

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -4,15 +4,8 @@ import search_icon from "./search_icon.svg";
 import cross_icon from "./cross_icon.svg";
 import upload_area from "./upload_area.svg";
 import sketch from "./sktech.svg";
-import course_1_thumbnail from "./course_1.png";
-import course_2_thumbnail from "./course_2.png";
-import course_3_thumbnail from "./course_3.png";
-import course_4_thumbnail from "./course_4.png";
 import star from "./rating_star.svg";
 import star_blank from "./star_dull_icon.svg";
-import profile_img_1 from "./girl1.jpeg";
-import profile_img_2 from "./boy.jpg";
-import profile_img_3 from "./girl2.jpg";
 import arrow_icon from "./arrow_icon.svg";
 import down_arrow_icon from "./down_arrow_icon.svg";
 import time_left_clock_icon from "./time_left_clock_icon.svg";
@@ -32,25 +25,14 @@ import dropdown_icon from "./dropdown_icon.svg";
 import patients_icon from "./patients_icon.svg";
 import play_icon from "./play_icon.svg";
 import blue_tick_icon from "./blue_tick_icon.svg";
-import course_4 from "./course_4.png";
-import profile_img from "./girl1 copy.jpeg";
-import profile_img2 from "./boy copy.jpg";
-import profile_img3 from "./girl2 copy.jpg";
 import lesson_icon from "./lesson_icon.svg";
 
 export const assets = {
   logo,
   search_icon,
   sketch,
-  course_1_thumbnail,
-  course_2_thumbnail,
-  course_3_thumbnail,
-  course_4_thumbnail,
   star,
   star_blank,
-  profile_img_1,
-  profile_img_2,
-  profile_img_3,
   arrow_icon,
   dropdown_icon,
   cross_icon,
@@ -67,14 +49,10 @@ export const assets = {
   facebook_icon,
   instagram_icon,
   twitter_icon,
-  course_4,
   file_upload_icon,
   appointments_icon,
   earning_icon,
   patients_icon,
-  profile_img,
-  profile_img2,
-  profile_img3,
   play_icon,
   blue_tick_icon,
   lesson_icon,
